0a72790a6e1c06422cd9ad3c621795f12e4b9474
d_z_4 <lunap@tuta.io>
2026-02-15 04:03:39 +0000

    feat: add permission checks

--- src/main/kotlin/luna/nodes/combat/commands/CombatAdminCommand.kt
+++ src/main/kotlin/luna/nodes/combat/commands/CombatAdminCommand.kt
@@ -11,12 +11,17 @@ package luna.nodes.combat.commands
 
 import luna.nodes.combat.commands.arguments.ArgumentItem
 import luna.nodes.combat.utils.Message
+import luna.nodes.nodes.utils.hasPermission
 import net.minestom.server.command.builder.Command
 import net.minestom.server.entity.Player
 
 class CombatAdminCommand : Command("combatadmin", "ca") {
     init {
         setDefaultExecutor { sender, context ->
+            if (!hasPermission((sender as Player), "combat.admin")) {
+                return@setDefaultExecutor
+            }
+
             Message.print(sender, "[Combat] Admin commands:")
             Message.print(sender, "/combatadmin give$: Give yourself various items")
         }
@@ -28,13 +33,21 @@ class CombatAdminCommand : Command("combatadmin", "ca") {
 class CombatAdminGiveCommand : Command("give") {
     init {
         setDefaultExecutor { sender, context ->
+            if (!hasPermission((sender as Player), "combat.admin")) {
+                return@setDefaultExecutor
+            }
+
             Message.print(sender, "Usage: /combatadmin give <item-name>")
         }
 
         val itemArg = ArgumentItem.create("item-name")
 
         addSyntax({ sender, context ->
-            (sender as Player).inventory.addItemStack(context[itemArg].toItemStack())
+            if (!hasPermission((sender as Player), "combat.admin")) {
+                return@addSyntax
+            }
+
+            sender.inventory.addItemStack(context[itemArg].toItemStack())
         }, itemArg)
     }
 }
