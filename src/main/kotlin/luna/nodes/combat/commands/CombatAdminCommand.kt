2835722439d2bc7ddad0ecdfbf68fde28c70e3a6
d_z_4 <lunap@tuta.io>
2026-03-02 20:48:30 +0000

    feat: explosions

--- src/main/kotlin/luna/nodes/combat/commands/CombatAdminCommand.kt
+++ src/main/kotlin/luna/nodes/combat/commands/CombatAdminCommand.kt
@@ -10,9 +10,11 @@
 package luna.nodes.combat.commands
 
 import luna.nodes.combat.commands.arguments.ArgumentItem
+import luna.nodes.combat.objects.Explosion
 import luna.nodes.combat.utils.Message
 import luna.nodes.nodes.utils.hasPermission
 import net.minestom.server.command.builder.Command
+import net.minestom.server.command.builder.arguments.ArgumentType
 import net.minestom.server.entity.Player
 
 class CombatAdminCommand : Command("combatadmin", "ca") {
@@ -23,10 +25,12 @@ class CombatAdminCommand : Command("combatadmin", "ca") {
             }
 
             Message.print(sender, "[Combat] Admin commands:")
-            Message.print(sender, "/combatadmin give$: Give yourself various items")
+            Message.print(sender, "/combatadmin give: Give yourself various items")
+            Message.print(sender, "/combatadmin explosion: Make an explosion")
         }
 
         addSubcommand(CombatAdminGiveCommand())
+        addSubcommand(CombatAdminExplosionCommand())
     }
 }
 
@@ -49,5 +53,28 @@ class CombatAdminGiveCommand : Command("give") {
 
             sender.inventory.addItemStack(context[itemArg].toItemStack())
         }, itemArg)
+    }
+}
+
+class CombatAdminExplosionCommand : Command("explosion") {
+    init {
+        setDefaultExecutor { sender, context ->
+            if (!hasPermission((sender as Player), "combat.admin")) {
+                return@setDefaultExecutor
+            }
+
+            Message.print(sender, "Usage: /combatadmin explosion <radius> <fire>")
+        }
+
+        val radisuArg = ArgumentType.Integer("radius")
+        val fireArg = ArgumentType.Double("fire")
+
+        addSyntax({ sender, context ->
+            if (!hasPermission((sender as Player), "combat.admin")) {
+                return@addSyntax
+            }
+
+            Explosion(sender.instance, sender.position, context[radisuArg], context[fireArg])
+        }, radisuArg, fireArg)
     }
 }
